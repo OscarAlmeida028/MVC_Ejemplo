6d5add8614df7b0649013a415dff466917762852
laboratorio <laboratorio@UPOAULA10506>
2023-09-27 09:45:18 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1_Ejemplo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1_Ejemplo.Models;
+
+namespace WebApplication1_Ejemplo.Utils
+{
+    public class Utils
+    {
+        static public List<Producto> ListaProductos = new List<Producto>()
+        {
+            new Producto(1, "Producto1", "Descripcion1", 1),
+            new Producto(1, "Producto2", "Descripcion2", 2),
+            new Producto(1, "Producto3", "Descripcion3", 3),
+            new Producto(1, "Producto4", "Descripcion4", 4),
+        };
+        
+        
+    }
+}
